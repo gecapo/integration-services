4ba3355fdbd755c470eb04cb592897758fb0c75e
Georgi Kalinkov <georgi.kalinkov@paynetics.digital>
2023-06-16 15:25:25 +0300

    Add project files.

---
+++ IntergrationServices.Worker/Program.cs
@@ -0,0 +1,10 @@
+using IntergrationServices.Worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
